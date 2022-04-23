6ae4ec13e6d3b8db1b13e9419abf9c411077db46
hesam attari <Hesam_att@outlook.com>
2022-04-23 16:36:51 +0430

    Crud is finished

--- Mc2.CrudTest.Presentation/Server/Controllers/CustomerController.cs
+++ Mc2.CrudTest.Presentation/Server/Controllers/CustomerController.cs
@@ -1,40 +1,45 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Infrastructure.Models.Customer;
 namespace Mc2.CrudTest.Presentation.Server.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class CustomerController : ControllerBase
     {
-        private readonly ICustomerService _personService;
-        public CustomerController(ICustomerService personService)
+        private readonly ICustomerService _customerService;
+        public CustomerController(ICustomerService customerService)
         {
-            _personService = personService;
+            _customerService = customerService;
         }
         [HttpGet]
-        public async Task<List<Person>> GetAll()
+        public async Task<List<Customer>> GetAll()
         {
-            return await _personService.GetAllPersons();
+            return await _customerService.GetAllPersons();
         }
         [HttpGet("{id}")]
-        public async Task<Person> Get(int id)
+        public async Task<Customer> Get(int id)
         {
-            return await _personService.GetPerson(id);
+            return await _customerService.GetPerson(id);
         }
         [HttpPost]
-        public async Task<Person> AddPerson([FromBody] Person person)
+        public async Task<Customer> AddPerson([FromBody] Person person)
         {
-            return await _personService.AddPerson(person);
+            return await _customerService.AddPerson(person);
         }
         [HttpDelete("{id}")]
         public async Task<bool> DeletePerson(int id)
         {
-            await _personService.DeletePerson(id);
+            await _customerService.DeletePerson(id);
             return true;
         }
         [HttpPut("{id}")]
-        public async Task<bool> UpdatePerson(int id, [FromBody] Person Object)
+        public async Task<bool> UpdatePerson(int id, [FromBody] Customer Object)
         {
-            await _personService.UpdatePerson(id, Object);
+            await _customerService.UpdatePerson(id, Object);
             return true;
         }
+
     }
 }
---
+++ Mc2.CrudTest.Presentation/Shared/CustomerViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mc2.CrudTest.Shared
+{
+    public class CustomerViewModel
+    {
+		public int Id { get; set; }
+		[Required]
+		[Display(Name = "First Name")]
+		public string Firstname { get; set; }
+		[Required]
+		[Display(Name = "Last Name")]
+		public string Lastname { get; set; }
+		[Required]
+		[Display(Name = "Date of Birth")]
+		public DateTime? DateOfBirth { get; set; }
+		[Required]
+		[Display(Name = "PhoneNumber")]
+		public string PhoneNumber { get; set; }
+		[Required]
+		[Display(Name = "Email")]
+		public string Email { get; set; }
+		[Required]
+		[Display(Name = "BankAccountNumber")]
+		public string BankAccountNumber { get; set; }
+	}
+}
